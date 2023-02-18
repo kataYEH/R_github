0e455384e48c5c64b850bbb624dc1a74a82814fc
kataYEH <xxszcvfh@gmail.com>
2023-02-18 11:20:50 +0800

    first
    first

---
+++ print1.r
@@ -0,0 +1,7 @@
+1 + 2       # calculator
+log(3.14)   # log function
+x = 1 + 2   # one plus two assign to x
+x           # print x
+x = c(1, 3, 5, 7) # get a vector
+mean(x)     # mean function
+log(x)      # log function
